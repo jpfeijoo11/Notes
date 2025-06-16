d7634a3150e79f0b6715a693e74a9e0c5a9b9105
Juan <juanpablote10@hotmail.com>
2025-06-15 20:39:03 -0500

    Todo se realizó del proyecto

--- Notes/Models/About.cs
+++ Notes/Models/About.cs
@@ -11,5 +11,5 @@ internal class About
     public string Title => AppInfo.Name;
     public string Version => AppInfo.VersionString;
     public string MoreInfoUrl => "https://aka.ms/maui";
-    public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    public string Message => "Hola, soy Juan Feijoo, desarrollador apasionado por la tecnología, el aprendizaje constante y la creación de soluciones digitales que generen impacto positivo. Me interesa el desarrollo de software multiplataforma, la inteligencia artificial y la innovación educativa. Esta aplicación es parte de mi camino como estudiante y creador, donde combino creatividad, lógica y ganas de seguir creciendo en el mundo del desarrollo. ¡Gracias por visitar esta sección y ser parte de esta experiencia!";
 }
